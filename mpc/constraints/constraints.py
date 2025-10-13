1a25f53c824d384eae74ccfb762deb40366c4dc3
Fengjun Yang <yangfengjun96@gmail.com>
2025-10-13 18:24:09 -0400

    change constraints file

--- mpc/constraints/constraints.py
+++ mpc/constraints/constraints.py
@@ -7,6 +7,7 @@ STANCE_HEIGHT_EPS = 0.05
 NO_SLIP_EPS = 0.1
 INF = 1e6
 
+
 def friction_cone_constraints(x_k, u_k, kindyn_model, config, contact_k):
     """Add friction cone constraints exactly like working Acados version."""
     # Key insight: The working version uses contact_sequence to DISABLE constraints
@@ -27,11 +28,14 @@ def friction_cone_constraints(x_k, u_k, kindyn_model, config, contact_k):
         # Normal force constraints
         expr_list.append(f_z)
         min_list.append(0)
-        max_list.append(config.grf_limits * contact_flag + SWING_GRF_EPS * (1 - contact_flag))  # grf <= EPS when not in contact
+        max_list.append(
+            config.robot_data.grf_limits * contact_flag
+            + SWING_GRF_EPS * (1 - contact_flag)
+        )  # grf <= EPS when not in contact
 
         # Friction cone constraints
         # Previously was not active for swing feet, but now is to enforce that swing feet should have zero GRF.
-        mu_term = config.mpc_params["mu"] * f_z
+        mu_term = config.experiment.mu_ground * f_z
 
         expr_list.append(f_x)
         min_list.append(-mu_term)
@@ -40,8 +44,9 @@ def friction_cone_constraints(x_k, u_k, kindyn_model, config, contact_k):
         expr_list.append(f_y)
         min_list.append(-mu_term)
         max_list.append(mu_term)
-    
+
     return cs.vertcat(*expr_list), cs.vertcat(*min_list), cs.vertcat(*max_list)
+
 
 def foot_height_constraints(x_k, u_k, kindyn_model, config, contact_k):
     """
@@ -61,24 +66,19 @@ def foot_height_constraints(x_k, u_k, kindyn_model, config, contact_k):
     H[0:3, 0:3] = w_R_b
     H[0:3, 3] = com_position
 
-    foot_height_fl = kindyn_model.forward_kinematics_FL_fun(
-        H, joint_positions
-    )[2, 3]
-    foot_height_fr = kindyn_model.forward_kinematics_FR_fun(
-        H, joint_positions
-    )[2, 3]
-    foot_height_rl = kindyn_model.forward_kinematics_RL_fun(
-        H, joint_positions
-    )[2, 3]
-    foot_height_rr = kindyn_model.forward_kinematics_RR_fun(
-        H, joint_positions
-    )[2, 3]
+    foot_height_fl = kindyn_model.forward_kinematics_FL_fun(H, joint_positions)[2, 3]
+    foot_height_fr = kindyn_model.forward_kinematics_FR_fun(H, joint_positions)[2, 3]
+    foot_height_rl = kindyn_model.forward_kinematics_RL_fun(H, joint_positions)[2, 3]
+    foot_height_rr = kindyn_model.forward_kinematics_RR_fun(H, joint_positions)[2, 3]
 
-    foot_heights = cs.vertcat(foot_height_fl, foot_height_fr, foot_height_rl, foot_height_rr)
+    foot_heights = cs.vertcat(
+        foot_height_fl, foot_height_fr, foot_height_rl, foot_height_rr
+    )
     foot_height_max = STANCE_HEIGHT_EPS * contact_k + INF * (1 - contact_k)
     foot_height_min = np.zeros(4)
 
     return foot_heights, foot_height_min, foot_height_max
+
 
 def foot_velocity_constraints(x_k, u_k, kindyn_model, config, contact_k):
     com_position = x_k[0:3]
@@ -109,32 +109,46 @@ def foot_velocity_constraints(x_k, u_k, kindyn_model, config, contact_k):
         qvel_joints_RR,
     )
 
-    foot_vel_FL = (
-        kindyn_model.jacobian_FL_fun(H, joint_positions)[0:3, :] @ qvel
-    )
-    foot_vel_FR = (
-        kindyn_model.jacobian_FR_fun(H, joint_positions)[0:3, :] @ qvel
-    )
-    foot_vel_RL = (
-        kindyn_model.jacobian_RL_fun(H, joint_positions)[0:3, :] @ qvel
-    )
-    foot_vel_RR = (
-        kindyn_model.jacobian_RR_fun(H, joint_positions)[0:3, :] @ qvel
-    )
+    foot_vel_FL = kindyn_model.jacobian_FL_fun(H, joint_positions)[0:3, :] @ qvel
+    foot_vel_FR = kindyn_model.jacobian_FR_fun(H, joint_positions)[0:3, :] @ qvel
+    foot_vel_RL = kindyn_model.jacobian_RL_fun(H, joint_positions)[0:3, :] @ qvel
+    foot_vel_RR = kindyn_model.jacobian_RR_fun(H, joint_positions)[0:3, :] @ qvel
 
     # Stack all foot velocities
-    foot_velocities = cs.vertcat(foot_vel_FL, foot_vel_FR, foot_vel_RL, foot_vel_RR) # Dimension: 4 x 3 = 12
-    foot_velocity_min = cs.repmat(-NO_SLIP_EPS * contact_k - INF * (1 - contact_k), 3, 1).reshape((-1, 1))  # Repeat for each xyz component
-    foot_velocity_max = cs.repmat(NO_SLIP_EPS * contact_k + INF * (1 - contact_k), 3, 1).reshape((-1, 1))  # Repeat for each xyz component
+    foot_velocities = cs.vertcat(
+        foot_vel_FL, foot_vel_FR, foot_vel_RL, foot_vel_RR
+    )  # Dimension: 4 x 3 = 12
+    foot_velocity_min = cs.repmat(
+        -NO_SLIP_EPS * contact_k - INF * (1 - contact_k), 3, 1
+    ).reshape((-1, 1))  # Repeat for each xyz component
+    foot_velocity_max = cs.repmat(
+        NO_SLIP_EPS * contact_k + INF * (1 - contact_k), 3, 1
+    ).reshape((-1, 1))  # Repeat for each xyz component
 
     return foot_velocities, foot_velocity_min, foot_velocity_max
+
 
 def joint_limits_constraints(x_k, u_k, kindyn_model, config, contact_k):
     # Add joint limits to prevent broken configurations
     joint_positions = x_k[12:24]  # 12 joint angles
-    return joint_positions, config.joint_limits_lower, config.joint_limits_upper
+    return (
+        joint_positions,
+        config.robot_data.joint_limits_lower,
+        config.robot_data.joint_limits_upper,
+    )
+
 
 def input_limits_constraints(x_k, u_k, kindyn_model, config, contact_k):
-    lb = np.concatenate((np.ones(12) * -config.joint_velocity_limits, np.ones(12) * -config.grf_limits))
-    ub = np.concatenate((np.ones(12) * config.joint_velocity_limits, np.ones(12) * config.grf_limits))
+    lb = np.concatenate(
+        (
+            np.ones(12) * -config.robot_data.joint_velocity_limits,
+            np.ones(12) * -config.robot_data.grf_limits,
+        )
+    )
+    ub = np.concatenate(
+        (
+            np.ones(12) * config.robot_data.joint_velocity_limits,
+            np.ones(12) * config.robot_data.grf_limits,
+        )
+    )
     return u_k, lb, ub
