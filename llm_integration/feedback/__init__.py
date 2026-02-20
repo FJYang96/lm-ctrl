1bc7190610d164901e53065c42b5368b751342a4
aroy12345 <aryanroy@seas.upenn.edu>
2026-02-20 06:05:49 -0500

    Remove old summarize_iteration code path from llm_evaluation
    Delete summarize_failed_iteration, summarize_successful_iteration,
    _format_metrics_brief, and the summarize_iteration wrapper now that the
    error handler uses generate_iteration_summary instead.

--- llm_integration/feedback/__init__.py
+++ llm_integration/feedback/__init__.py
@@ -8,7 +8,6 @@ from .llm_evaluation import (
     evaluate_iteration_unified,
     generate_iteration_summary,
     get_evaluator,
-    summarize_iteration,
 )
 from .reference_feedback import generate_reference_feedback
 from .video_extraction import create_visual_feedback
@@ -18,7 +17,6 @@ __all__ = [
     "format_hardness_report",
     "evaluate_iteration_unified",
     "generate_iteration_summary",
-    "summarize_iteration",
     "get_evaluator",
     "generate_constraint_feedback",
     "generate_reference_feedback",
