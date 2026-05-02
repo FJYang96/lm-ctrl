831afc127e77bb0bad0474cd2c527d45cbb847d6
Fengjun Yang <yangfengjun96@gmail.com>
2026-05-02 14:12:05 -0400

    slight change in the run_local script

--- run_local.sh
+++ run_local.sh
@@ -20,12 +20,13 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # PYTHON="/home/aryanroy/miniconda3/bin/python3"
 
 # Non-interactive matplotlib
 # conda init
-# conda activate quadruped_pympc_ros2_jazzy_env
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate quadruped_pympc_ros2_jazzy_env
 source .env
 export MPLBACKEND=Agg
 # Use EGL for headless OpenGL rendering (avoids gladLoadGL errors)
